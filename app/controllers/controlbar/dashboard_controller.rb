b4565599ed682e3ccd40652189c8626a2f665a03
evanchawu <evan.cha.wu@gmail.com>
2015-04-15 22:08:50 +0800

    add backend link bottom to navbar with Cancan

--- app/controllers/controlbar/dashboard_controller.rb
+++ app/controllers/controlbar/dashboard_controller.rb
@@ -3,6 +3,13 @@ class Controlbar::DashboardController < ApplicationController
   layout 'backend'
   before_action :authenticate_user!	
   
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
+
   def index
+  	authorize! :manage, :all
   end
 end
